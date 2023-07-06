f8bd56892661a3d4f7bdc8774d87632da1837902
wanuii <judy890226@gmail.com>
2023-07-06 14:13:57 +0800

    14:13

--- client/src/components/MemberCentre/Centre.jsx
+++ client/src/components/MemberCentre/Centre.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import miscImage from '../../assets/img/misc/wave.jpg';
 
-function Ordertracking() {
+function Centre() {
   const divStyle = {
     position: 'relative',
     width: '100%',
     height: '100vh',
   };
-
   const overlayStyle = {
     position: 'absolute',
     top: 0,
@@ -20,20 +19,15 @@ function Ordertracking() {
     backgroundPosition: 'center',
     opacity: 0.5,
   };
-
   return (
     <div style={divStyle}>
       <div style={overlayStyle}></div>
-      <div id="centre">
-        訂單追蹤
+      <div>
+        會員中心
       </div>
+      
     </div>
   );
 }
 
-export default Ordertracking;
-
-
-// order跟cmmgmt是不同page，所以把背景圖跟|會員中心|放在這裡(MemberCentre內)，到時候再分別import到order跟cmmgmt裡面
-// 然後訂單追蹤放order page 商品管理放cmmgmt page
-// 兩個頁面的元件再分別放在component各自的資料夾中，最終導入order page或cmmgmt page
+export default Centre;
--- client/src/page/order.jsx
+++ client/src/page/order.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
-import Ordertracking from '../components/MemberCentre/ordertracking';
-import miscImage from '../assets/img/misc/wave.jpg';
+import Centre from '../components/MemberCentre/Centre';
 
 function Order() {
-  const divStyle = {
-    position: 'relative',
-    width: '100%',
-    height: '100vh',
-  };
-  const overlayStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundImage: `url(${miscImage})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    opacity: 0.5,
-  };
+  
   return (
-    <div style={divStyle}>
-      <div style={overlayStyle}></div>
-      <div id="centre">
-        會員中心
-        <Ordertracking />
-      </div>
-      
-    </div>
+    <>
+    <Centre />
+    </>
   );
 }
 
