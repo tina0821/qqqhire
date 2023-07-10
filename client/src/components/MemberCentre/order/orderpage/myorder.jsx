3fe71fa1cbc638a91a224a39388a1e5c8067efc0
wanuii <judy890226@gmail.com>
2023-07-10 09:11:47 +0800

    資料提出來

--- client/src/components/MemberCentre/order/orderpage/myorder.jsx
+++ client/src/components/MemberCentre/order/orderpage/myorder.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../order.scss';
 
 function Myorder() {
+  const [tradeitems, setTradeitems] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/tradeitems');
+      const tradeitems = response.data;
+      console.log(tradeitems);
+      setTradeitems(tradeitems);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      hi myorder
+      {tradeitems.map((tradeitem) => (
+        <div key={tradeitem.tradeitemId} className="trade-item">
+          <div>
+            <h4>訂單編號</h4>
+            <p>{tradeitem.tradeitemId}</p>
+          </div>
+          <div>
+            <h4>商品</h4>
+            <p>{tradeitem.productName}</p>
+          </div>
+          <div>
+            <h4>訂單狀態</h4>
+            <p>
+              {tradeitem.state === 0
+                ? '等待回應中'
+                : tradeitem.state === 1
+                ? '等待租借中'
+                : tradeitem.state}
+            </p>
+          </div>
+          <hr />
+        </div>
+      ))}
     </div>
   );
+  
 }
 
 export default Myorder;
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ var sha1 = require('sha1');
 var querystring = require('querystring');
 var coon = require('./routes/db');
 var app=express();
+app.use(cors());
 app.use(urlencoded);
 app.use('/img',express.static('public/img'))
 app.get('/',function(req,res){
@@ -16,7 +17,67 @@ app.get('/',function(req,res){
 app.get('/cart',function(req,res){
     res.send('cartInfo');
 });
+// app.get('/api/tradeitem', function(req, res) {
+//   const query = 'SELECT tradeitemId FROM tradeitem';
+//   coon.query(query, function(error, results) {
+//     if (error) {
+//       console.log(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } else {
+//       res.json(results);
+//     }
+//   });
+// });
+
+// ...其餘程式碼
+
+app.get('/api/tradeitems', function(req, res) {
+  const query = 'SELECT tradeitemId, productId, state FROM tradeitem';
+  coon.query(query, function(error, results) {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      if (results.length > 0) {
+        const productIds = results.map((result) => result.productId);
+        const query = 'SELECT productId, productName FROM product WHERE productId IN (?)';
+        coon.query(query, [productIds], function(error, productResults) {
+          if (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+          } else {
+            const tradeitems = results.map((result) => {
+              const product = productResults.find((product) => product.productId === result.productId);
+              return {
+                tradeitemId: result.tradeitemId,
+                productName: product ? product.productName : 'Product not found',
+                state: result.state,
+              };
+            });
+            res.json(tradeitems);
+          }
+        });
+      } else {
+        res.json([]);
+      }
+    }
+  });
+});
+
+// ...其餘程式碼
+
+
+// ...其餘程式碼
+
+
+
+// app.get('/api/tradeitem', (req, res) => {
+//     const qur = 'SELECT * FROM tradeitem WHERE 1'
+//     coon.query(qur, (err, data) => {
+//         res.json(data)
+//     })
+// })
 app.listen(8000,function(){
-    console.clear();
+    // console.clear();
     console.log(new Date().toLocaleDateString());
 });
