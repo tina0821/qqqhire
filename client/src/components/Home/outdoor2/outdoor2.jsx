c3d11bd904b5f674ded24b2b106f10c3ed2b70ce
ads88364 <s410001638@gmail.com>
2023-07-12 01:19:04 +0800

    7/12 01:20

--- client/src/components/Home/outdoor2/outdoor2.jsx
+++ client/src/components/Home/outdoor2/outdoor2.jsx
@@ -1,18 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useEffect } from 'react';
 import "./outdoor2.css"
 
 function Outdoor2() {
+    const od2AmiRL = useRef(null);
+    const od2AmiRight = useRef(null);
+    const od2AmiLeft = useRef(null);
+
+
+    useEffect(() => {
+
+        od2AmiLeft.current.addEventListener('mouseenter', (e) => {
+            od2AmiRL.current.classList.add('hover-right')
+            // console.log("AAA")
+        })
+        od2AmiLeft.current.addEventListener('mouseleave', (e) => {
+            od2AmiRL.current.classList.remove('hover-right')
+            // console.log("AAA By")
+        });
+
+        od2AmiRight.current.addEventListener('mouseenter', (e) => {
+            od2AmiRL.current.classList.add('hover-left')
+            // console.log("BBB")
+        })
+        od2AmiRight.current.addEventListener('mouseleave', (e) => {
+            od2AmiRL.current.classList.remove('hover-left')
+            // console.log("BBB By")
+        });
+
+    });
+
+
+
     return (
         <>
             {/* 主頁畫面2 出租/租借 */}
-            <div className="outdoor2 blur ">
-                <div className="outdoor2box1" />
-                <div className="split left">
+            <div className="outdoor2 blur" ref={od2AmiRL}>
+
+                <div className="outdoor2box1"></div>
+
+                <div className="split left" ref={od2AmiRight}>
                     <h1>出租商品</h1>
                     <a href className="btn-100">explore</a>
                     {/* <p>與他人分享你的愛好,同時賺取額外收入</p> */}
                 </div>
-                <div className="split right">
+
+                <div className="split right" ref={od2AmiLeft}>
                     <h1>租借商品</h1>
                     <a href className="btn-100">explore</a>
                     {/* <p>與他人分享你的愛好,同時賺取額外收入</p> */}
