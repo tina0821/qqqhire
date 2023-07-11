468b6e79267035813c704cbe406781361c99e3f5
ads88364 <s410001638@gmail.com>
2023-07-11 17:28:19 +0800

    7/11 17:28

--- client/src/components/Home/navbar/navbar.jsx
+++ client/src/components/Home/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useEffect } from 'react';
 import "./navbar.css";
 
 // 引入 Font Awesome icon
@@ -11,40 +11,53 @@ import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 
 function Navbar() {
+    const headerBGC = useRef(null)
+    
+    useEffect(() => {
+        window.addEventListener( 'scroll',(e) => {
+            if (window.scrollY>0) {
+                headerBGC.current.classList.add('changeBGColor')
+            }else{
+                headerBGC.current.classList.remove('changeBGColor') 
+            }
+
+        })
+    })
+
+
     return (
         <>
             {/* 標題列 */}
-            <header className="header">
+            <header className="header" ref={headerBGC}>
                 {/* LOGO */}
                 <div className="header-logo">
-                    {/* LOGO超連結? */}
-                    <a href>
-                        {/* LOGO 用圖片還是文字? */}
+                    {/* LOGO超連結 */}
+                    <a className="headerLogoImg" href = "">
+                        {/* 白 */}
                         <img src="http://localhost:8000/img/home/Logo%E7%99%BD.png" />    
+                        {/* 黑 */}
+                        <img src="http://localhost:8000/img/home/Logo%E9%BB%91.png" />    
                     </a>
                 </div>
                 {/* 右邊按鈕/icon */}
                 <div className="header-button-icon">
                     {/* 按鈕 */}
                     <ul className="header-button">
-                        <li><a button className="btn-98" href>租借列表</a></li>
-                        <li><a button className="btn-98" href>快速上架</a></li>
-                        <li><a button className="btn-98" href>關於我們</a></li>
+                        <li><a button className="btn-98"  href>租借列表</a></li>
+                        <li><a button className="btn-98"  href>快速上架</a></li>
+                        <li><a button className="btn-98"  href>關於我們</a></li>
                     </ul>
                     {/* icon */}
                     <ul className="header-icon">
-                        <li><a href className="iconMove"><FontAwesomeIcon icon={faHeart} className="icon"/></a></li>
-                        <li><a href className="iconMove"><FontAwesomeIcon icon={faCartShopping} className="icon"/></a></li>
+                        <li><a href className="iconMove" ><FontAwesomeIcon icon={faHeart} className="icon"/></a></li>
+                        <li><a href className="iconMove" ><FontAwesomeIcon icon={faCartShopping} className="icon"/></a></li>
                         <li>
-                            <a href className="iconMove"><FontAwesomeIcon icon={faCircleUser} className="icon"/></a>
+                            <a href className="iconMove" ><FontAwesomeIcon icon={faCircleUser} className="icon"/></a>
                             <ul className="drop-menu limenu-2">
-                                <li><a href>會員登入</a></li>
-                                <li><a href>會員註冊</a></li>
+                                <li><a href className="iconList">會員登入</a></li>
+                                <li><a href className="iconList">會員註冊</a></li>
                             </ul>
                         </li>
-                        {/* 測試icon變化 */}
-                        {/* 目前icon圖案太粗 要設定細框需要再找問題 */}
-                        {/* <li><i class="fa-thin fa-cart-shopping" style="color: #ffffff;" aria-hidden="true"></i></li> */}
                     </ul>
                 </div>
             </header>
--- client/src/components/Home/outdoor/outdoor.jsx
+++ client/src/components/Home/outdoor/outdoor.jsx
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useRef } from 'react';
 import "./outdoor.css"
 
 
 function Outdoor() {
+    const odAmiCycle = useRef(null);
+    const odAmiArrow = useRef(null);
+
+    useEffect(() => {
+        odAmiCycle.current.addEventListener('mouseenter', (e) => {
+            // 圓形動畫
+            odAmiCycle.current.classList.add('menu2-animation')
+            odAmiCycle.current.classList.remove('menu3-animation')
+            // 箭頭痛畫
+            odAmiArrow.current.classList.add('menu4-animation')
+            odAmiArrow.current.classList.remove('menu5-animation')
+        })
+        odAmiCycle.current.addEventListener('mouseleave', (e) => {
+            // 圓形動畫
+            odAmiCycle.current.classList.remove('menu2-animation')
+            odAmiCycle.current.classList.add('menu3-animation')
+            // 箭頭動畫
+            odAmiArrow.current.classList.remove('menu4-animation')
+            odAmiArrow.current.classList.add('menu5-animation')
+        })
+    })
+
+
     return (
         <>
             {/* 主頁畫面 */}
             <div className="outdoor">
-                <img src="http://localhost:8000/img/home/homepage/surf1.jpg"/>
+                <img src="http://localhost:8000/img/home/homepage/surf1.jpg" />
                 <div className="outdoor-word">
                     <p>Hire outdoor</p>
                     <p>海爾戶外</p>
                 </div>
                 <a href className="outdoor-btn">
                     <img src="http://localhost:8000/img/home/hirecircle.png" className="hireClass" id="hireId" />
-                    <div className="btn-div2class" id="btn-div2">
-                        <p className="btn-pClass" id="btn-p">↓</p>
-                    </div>
+                    <div className="btn-div2class" id="btn-div2" ref={odAmiCycle}></div>
+                    <p className="btn-pClass" id="btn-p" ref={odAmiArrow}>↓</p>
                 </a>
             </div>
         </>
