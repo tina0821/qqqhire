51709a186d3a210ded106e0db4a858270c439a02
wanuii <judy890226@gmail.com>
2023-07-19 17:06:13 +0800

    0719

--- client/src/components/MemberCentre/order/orderpage/historyorder.jsx
+++ client/src/components/MemberCentre/order/orderpage/historyorder.jsx
@@ -1,20 +1,17 @@
+// Historyorder.jsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import OrderDetail from './Orderdetail';
+import Ordertable from './ordertable';
+import { getOrderStatus } from './orderutils'; // 引入共用函式
 
 const Historyorder = () => {
   const [tradeItems, setTradeItems] = useState([]);
   const [selectedTradeItemId, setSelectedTradeItemId] = useState(null);
   const [showOrderDetail, setShowOrderDetail] = useState(false);
-  // const a = localStorage.getItem('userInfo').slice(1,-1)
 
   const useract = localStorage.getItem('userInfo');
-  // 確認 userInfo 的值不是空值（null）再進行 slice 操作
   const user = useract ? useract.slice(1, -1) : '';
-
-  useEffect(() => {
-    fetchTradeItems();
-  }, []);
 
   const fetchTradeItems = async () => {
     try {
@@ -25,19 +22,9 @@ const Historyorder = () => {
     }
   };
 
-  const getOrderStatus = (state) => {
-    if (state === 0) {
-      return '等待回應中';
-    } else if (state === 1) {
-      return '等待租借中';
-    } else if (state === 2) {
-      return '租借中';
-    } else if (state === 3) {
-      return '已完成';
-    } else if (state === 4) {
-      return '已取消';
-    }
-  };
+  useEffect(() => {
+    fetchTradeItems();
+  }, []);
 
   const handleDetail = (tradeItemId) => {
     setSelectedTradeItemId(tradeItemId);
@@ -49,87 +36,14 @@ const Historyorder = () => {
     setShowOrderDetail(false);
   };
 
-  const limitProductName = (productName) => {
-    const maxChars = 6;
-    if (productName.length <= maxChars) {
-      return productName;
-    }
-    const truncated = productName.substr(0, maxChars);
-    const remainder = productName.substr(maxChars);
-    return (
-      <>
-        {truncated}
-        <br />
-        {remainder}
-      </>
-    );
-  };
-
-  const calculateDays = (rentStart, rentEnd) => {
-    const start = new Date(rentStart);
-    const end = new Date(rentEnd);
-    const timeDiff = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
-    return timeDiff;
-  };
-
-  const uniqueTradeItems = tradeItems.filter(
-    (tradeItem, index, self) => self.findIndex((t) => t.tradeitemId === tradeItem.tradeitemId) === index
-  );
-
-  if (showOrderDetail) {
-    return (
-      <OrderDetail tradeitemId={selectedTradeItemId} tradeitems={tradeItems} handleBack={handleBack} />
-    );
-  }
+  const filterState = (state) => state > 2; // 過濾狀態為大於2的訂單
 
   return (
     <div className="order-component">
-      {selectedTradeItemId ? (
-        <OrderDetail tradeitemId={selectedTradeItemId} tradeitems={tradeItems} />
+      {showOrderDetail ? (
+        <OrderDetail tradeitemId={selectedTradeItemId} tradeitems={tradeItems} handleBack={handleBack} />
       ) : (
-        <table className="order-table">
-          <thead>
-            <tr>
-              <th>訂單編號</th>
-              <th>商品</th>
-              <th>預約日期</th>
-              <th>歸還日期</th>
-              <th>天數</th>
-              <th>總金額</th>
-              <th>訂單狀態</th>
-              <th>操作</th>
-            </tr>
-          </thead>
-          <tbody>
-            {uniqueTradeItems.map((tradeItem) => {
-              if (tradeItem.state > 2) {
-                // 訂單狀態
-                const orderStatus = getOrderStatus(tradeItem.state);
-                const rentTotal = tradeItems
-                  .filter((item) => item.tradeitemId === tradeItem.tradeitemId)
-                  .reduce((total, item) => total + item.rent, 0);
-                const depositTotal = tradeItems
-                  .filter((item) => item.tradeitemId === tradeItem.tradeitemId)
-                  .reduce((total, item) => total + item.deposit, 0);
-                return (
-                  <tr id="trtd" key={tradeItem.tradeitemId}>
-                    <td>{tradeItem.tradeitemId}</td>
-                    <td>{limitProductName(tradeItem.productName)}</td>
-                    <td>{new Date(tradeItem.rentStart).toLocaleDateString()}</td>
-                    <td>{new Date(tradeItem.rentEnd).toLocaleDateString()}</td>
-                    <td>{calculateDays(tradeItem.rentStart, tradeItem.rentEnd)}</td>
-                    <td>{calculateDays(tradeItem.rentStart, tradeItem.rentEnd) * rentTotal + depositTotal}</td>
-                    <td>{orderStatus}</td>
-                    <td>
-                      <button id='morebtn' onClick={() => handleDetail(tradeItem.tradeitemId)}>詳細</button>
-                    </td>
-                  </tr>
-                );
-              }
-              return null;
-            })}
-          </tbody>
-        </table>
+        <Ordertable tradeItems={tradeItems} filterState={filterState} handleDetail={handleDetail} />
       )}
     </div>
   );
