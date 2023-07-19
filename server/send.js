57a84e202aeb6e50d7872d6f882c0ff8f214eddd
ads88364 <s410001638@gmail.com>
2023-07-19 10:40:08 +0800

    修改寄信HTML樣式

--- server/send.js
+++ server/send.js
@@ -1,8 +1,4 @@
-// require('@babel/register');
-
 const nodemailer = require("nodemailer");
-// const { renderToString } = require("react-dom/server");
-// const Returnqu = require("../client/src/components/returnqu/returnqu");
 
 
 const transporter = nodemailer.createTransport({
@@ -14,21 +10,94 @@ const transporter = nodemailer.createTransport({
 });
 
 
-// const reactElement = <Returnqu />;
-// const reactHtml = renderToString(reactElement);
 
 
 const mailOptions = {
    from: "hireoutdoor2023@gmail.com",
    to: "hireoutdoor2023@gmail.com",
    subject: "Nodemailer Test",
-   html: ""
+   html: `
+
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        
+      @import url(https://fonts.googleapis.com/css?family=Lato:100,300,400);
+    
+      body {
+         font-family: 'Lato', sans-serif;
+         background: #e2dedb;
+         color: #b3aca7;
+      }
+
+      header {
+         position: relative;
+         margin: 100px 0 25px 0;
+         font-size: 2.3em;
+         text-align: center;
+         letter-spacing: 7px;
+      }
+
+      #form {
+         position: relative;
+         width: 500px;
+         margin: 50px auto 100px auto;
+      }
+
+      input {
+         font-family: 'Lato', sans-serif;
+         font-size: 0.875em;
+         width: 470px;
+         height: 50px;
+         padding: 0px 15px 0px 15px;
+
+         background: transparent;
+         outline: none;
+         color: #726659;
+
+         border: solid 1px #b3aca7;
+         border-bottom: none;
+      }
+  
+      textarea {
+         width: 470px;
+         max-width: 470px;
+         height: 110px;
+         max-height: 110px;
+         padding: 15px;
+
+         background: transparent;
+         outline: none;
+
+         color: #726659;
+         font-family: 'Lato', sans-serif;
+         font-size: 0.875em;
+
+         border: solid 1px #b3aca7;
+      }
+
+    </style>
+   </head>
+
+   <body>
+    <header>PROBLEM REPORT</header>
+    <form id="form" class="topBefore">
+        <input id="name" type="text" placeholder="NAME" value="John DoeXXX" readonly>
+        <input id="email" type="text" placeholder="E-MAIL" value="johndoeXXX@example.com" readonly>
+        <textarea id="message" type="text" placeholder="MESSAGE" readonly>測試用 客訴XXXXXXX</textarea>
+    </form>
+   </body>
+   </html>
+
+   `
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-   if(error){
+transporter.sendMail(mailOptions, function (error, info) {
+   if (error) {
       console.log(error);
-   }else{
+   } else {
       console.log("Email sent: " + info.response + '成功發送');
    }
 });
