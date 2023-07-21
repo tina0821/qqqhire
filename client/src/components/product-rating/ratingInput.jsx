92bb5e6d0b0b848b3beb8e0a1678c4505cf80690
chaos90343 <chaos90343@gmail.com>
2023-07-21 15:48:09 +0800

    gg

--- client/src/components/product-rating/ratingInput.jsx
+++ client/src/components/product-rating/ratingInput.jsx
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import RatingStar from './ratingstar';
 
+
 import './ratingInput.scss'
 
 function RatingInput() {
+  const [openclose,setopenclose] = useState(0)
+  const [rating, setRating] = useState(0); 
+
+  
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+  };
+
+  const openrating = (state) =>{
+    setopenclose(state)
+  }
+
+
+
   return ( 
     <>
-      <div className='ratinginput'>
-        <div className='RI-title'>
-          <span>給此商品的評論</span>
-          <button>X</button>
-        </div>
-        <div>
-          <RatingStar/>
-        </div>
+      <button onClick={()=>{openrating(1)}}>給評</button>
+      {openclose===1?(
+        <div className='ratinginput'>
+          <div className='RI-title'>
+            <span>給此商品的評論</span>
+            <button onClick={()=>{openrating(2)}}>X</button>
+          </div>
+          <div>
+          <RatingStar rating={rating} onRatingChange={handleRatingChange} />
+          </div>
 
-        <div className='RI-text'>
-          <textarea type="text" placeholder='請輸入給商品的評價(限30字內)' />
-        </div>
+          <div className='RI-text'>
+            <textarea type="text" placeholder='請輸入給商品的評價(限30字內)' />
+          </div>
 
-        <div className='RI-bottom'>
-          <button>取消</button>
-          <button>送出</button>
+          <div className='RI-bottom'>
+            <button onClick={()=>{openrating(2)}}>取消</button>
+            <button>送出</button>
+          </div>
         </div>
-      </div>
+      ):null}
     </>
    );
 }
