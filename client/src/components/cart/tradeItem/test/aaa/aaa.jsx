17ceddff99fe37f224d8176b176ebb59573937fd
chaos90343 <chaos90343@gmail.com>
2023-07-25 19:32:45 +0800

    gg

--- client/src/components/cart/tradeItem/test/aaa/aaa.jsx
+++
@@ -1,7 +0,0 @@
-export const createHiddenInput = (form, name, value) => {
-    const hiddenField = document.createElement('input');
-    hiddenField.type = 'hidden';
-    hiddenField.name = name;
-    hiddenField.value = value;
-    form.appendChild(hiddenField);
-  };
