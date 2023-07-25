8492cdc4fb768b6c40eee3018203ea8b1473a865
chaos90343 <chaos90343@gmail.com>
2023-07-25 19:57:11 +0800

    7

--- client/src/components/cart/socket/socket.jsx
+++ client/src/components/cart/socket/socket.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {redirect} from 'react-router-dom'
+import { redirect } from 'react-router-dom'
 import { Row, Col, ConfigProvider, Button } from "antd";
 import axios from "axios";
 import ChatNavBar from "./ChatNavBar/ChatNavBar";
@@ -10,7 +10,7 @@ import { ChatFooter } from "./ChatFooter/ChatFooter";
 const socket = webSocket.connect("http://localhost:9000");
 
 
-export const Chat = ({setShow}) => {
+export const Chat = ({ setShow }) => {
   return (
     <Button
       style={{
@@ -21,8 +21,8 @@ export const Chat = ({setShow}) => {
         position: "fixed",
         right: "5px",
         bottom: "5px",
-        fontSize:"1.7rem",
-        zIndex:1000
+        fontSize: "1.7rem",
+        zIndex: 1000
       }}
       size="large"
       className="ms-3 btnColor cartFontSize"
@@ -35,40 +35,40 @@ export const Chat = ({setShow}) => {
   );
 };
 
-export const Main = ({chatInfo="",showRoom="" }) => {
-  const [show,setShow] = useState("")
+export const Main = ({ chatInfo = "", showRoom = "" }) => {
+  const [show, setShow] = useState("")
   const [roomName, setRoomName] = useState("");
-  const [productAccount,setProductAccount]= useState("")
+  const [productAccount, setProductAccount] = useState("")
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [removeRoom ,setRemoveRoom] = useState("")
+  const [removeRoom, setRemoveRoom] = useState("")
 
-  localStorage.getItem("userInfo")?(
+  localStorage.getItem("userInfo") ? (
     localStorage.setItem(
       "userName",
       localStorage.getItem("userInfo").slice(1, -1)
     )
-  ):( redirect('/login'))
+  ) : (redirect('/login'))
 
 
   useEffect(() => {
-    if(chatInfo.trim()){
-       axios
-      .post("http://localhost:8000/cart/chatInfo", {
-        account: localStorage.getItem("userInfo").slice(1, -1).trim(),
-        productAccount: chatInfo,
-      })
-      .then((res) => {
-        setRoomName(res.data)
-        setShow(showRoom)
-        setProductAccount(chatInfo)
-      });
+    if (chatInfo.trim()) {
+      axios
+        .post("http://localhost:8000/cart/chatInfo", {
+          account: localStorage.getItem("userInfo").slice(1, -1).trim(),
+          productAccount: chatInfo,
+        })
+        .then((res) => {
+          setRoomName(res.data)
+          setShow(showRoom)
+          setProductAccount(chatInfo)
+        });
     }
-  },[chatInfo,showRoom])
+  }, [chatInfo, showRoom])
 
   useEffect(() => {
     setMessages([])
-  },[roomName])
+  }, [roomName])
 
   socket.emit("newUser", {
     userName: localStorage.getItem("userInfo").slice(1, -1),
@@ -76,48 +76,48 @@ export const Main = ({chatInfo="",showRoom="" }) => {
   });
 
   return (
-    show?(
+    show ? (
       <ConfigProvider
-      theme={{
-        token: {
-          fontSize: "2rem",
-        },
-      }}
-      >
-      <Row
-        className="m-5"
-        style={{
-          border: "3px solid #0B7597",
-          borderRadius: "10px",
-          width: "600px",
-          position: "fixed",
-          right: "-45px",
-          bottom: "-45px",
-          zIndex: 1000,
+        theme={{
+          token: {
+            fontSize: "2rem",
+          },
         }}
+      >
+        <Row
+          className="m-5"
+          style={{
+            border: "3px solid #0B7597",
+            borderRadius: "10px",
+            width: "600px",
+            position: "fixed",
+            right: "-45px",
+            bottom: "-45px",
+            zIndex: 1000,
+          }}
         >
-        <Col span={24}>
-          <ChatNavBar setShow={setShow} productAccount={productAccount} />
-        </Col>
-        <Col style={{ borderTop: "3px solid #0B7597 " }} span={24}>
-          <Row>
-            <Col span={6} style={{ borderRight: "3px solid #0B7597" }}>
-              <ChatBar socket={socket} removeRoom={removeRoom} setMessages={setMessages} setRoomName={setRoomName} setProductAccount={setProductAccount} />
-            </Col>
-            <Col span={18}>
-              <Row>
-                <Col span={24}>
-                  <ChatBody productAccount={productAccount} setRemoveRoom={setRemoveRoom} socket={socket} setMessages={setMessages} messages={messages} roomName={roomName} />
-                </Col>
-                <Col span={24}>
-                  <ChatFooter socket={socket} message={message} setMessage={setMessage} roomName={roomName} />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </ConfigProvider>
-  ):<Chat setShow={setShow}/>
+          <Col span={24}>
+            <ChatNavBar setShow={setShow} productAccount={productAccount} />
+          </Col>
+          <Col style={{ borderTop: "3px solid #0B7597 " }} span={24}>
+            <Row>
+              <Col span={6} style={{ borderRight: "3px solid #0B7597" }}>
+                <ChatBar socket={socket} removeRoom={removeRoom} setMessages={setMessages} setRoomName={setRoomName} setProductAccount={setProductAccount} />
+              </Col>
+              <Col span={18}>
+                <Row>
+                  <Col span={24}>
+                    <ChatBody productAccount={productAccount} setRemoveRoom={setRemoveRoom} socket={socket} setMessages={setMessages} messages={messages} roomName={roomName} />
+                  </Col>
+                  <Col span={24}>
+                    <ChatFooter socket={socket} message={message} setMessage={setMessage} roomName={roomName} />
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </ConfigProvider>
+    ) : <Chat setShow={setShow} />
   );
 };
